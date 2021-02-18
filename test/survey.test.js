02d2bdf09d62eb60bb4676609d734500c8e82c3e
hh-kigcasan <hh.kigcasan@gmail.com>
2021-02-18 15:34:27 +0800

    Simplified test

--- test/survey.test.js
+++ test/survey.test.js
@@ -7,16 +7,6 @@ const SurveyModel           = require('../models/survey.model');
 
 describe("Survey Model", function(){
 
-    it('Should successfully insert survey form values', async function(){
-        let surveyModel = new SurveyModel();
-        await surveyModel.createSurvey("Oliver", "Philippines", "Node", "I love Node!");
-
-        let record = await surveyModel.getSurvey();
-
-        expect(record.status).to.equal(true);
-        expect(record.result).to.not.be.empty;
-    });
-
     it('Should return success message when input captcha matches.', function(){
         let surveyModel = new SurveyModel();
         let captcha = surveyModel.captcha;
